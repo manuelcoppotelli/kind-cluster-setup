8169264ee5e28b69ed245656f3eeed6cf0ac7539
Manuel Coppotelli <manuelcop@gmail.com>
2023-04-25 22:30:25 +0200

    Update quisk-setup.sh

--- quisk-setup.sh
+++ quisk-setup.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 docker run -d --restart=always -p "127.0.0.1:5000:5000" --name "kind-registry" registry:2
-kind create cluster --config=config.yaml
+curl https://raw.githubusercontent.com/manuelcoppotelli/kind-cluster-setup/main/cluster.yaml | kind create cluster --config -
 docker network connect kind kind-registry
 kubectl apply --filename tools/local-registry-hosting.yaml
